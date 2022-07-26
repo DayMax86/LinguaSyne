5aa908c48dec4b7773e36f361bd906e38c886e93
DayMax86 <daymax96@gmail.com>
2022-07-26 10:11:16 +0100

    WIP implementing coroutines to have animation display asynchronously

--- app/src/main/java/com/example/linguasyne/ReviewTermActivity.kt
+++ app/src/main/java/com/example/linguasyne/ReviewTermActivity.kt
@@ -2,12 +2,20 @@ package com.example.linguasyne
 
 import android.animation.ObjectAnimator
 import android.content.Intent
+import android.graphics.Path
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.addListener
+import androidx.core.widget.doAfterTextChanged
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 
@@ -24,53 +32,61 @@ class ReviewTermActivity : AppCompatActivity() {
         RevisionSessionManager.current_session.currentTerm = TermDisplayManager.termList[0]
         displayTerm()
 
-        findViewById<Button>(R.id.submit_button).setOnClickListener {
-
-            //Check if the answer is right
-            if (TermDisplayManager.checkAnswer(
-                    findViewById<EditText>(R.id.answerbox).text.toString()
-                )
-            ) {
-                //User got the answer correct so show appropriate animation
-                animateAnswer(true)
-                //Animation needs to complete before the UI is reset!
-                //Load the next term
-                TermDisplayManager.loadNextTerm()
-                resetUI()
-                //* Display next term
-                displayTerm()
-            } else {
-                //User got the answer wrong so show appropriate animation
-                animateAnswer(false)
+        val ansBox = findViewById<EditText>(R.id.answerbox)
+        ansBox.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                ansBox.setBackgroundResource(R.drawable.rounded_corners_textbox_bg)
             }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        findViewById<Button>(R.id.submit_button).setOnClickListener {
+            validateAnswer()
         }
     }
 
+    private fun ReviewTermActivity.validateAnswer() = runBlocking {
+        //Check if the answer is right
+        if (TermDisplayManager.checkAnswer(
+                findViewById<EditText>(R.id.answerbox).text.toString()
+            )
+        ) {
+            //User got the answer correct so show appropriate animation
+            animateAnswer(true)
+            //Animation needs to complete before the UI is reset!
+            //Load the next term
+            advance()
+            resetUI()
+            //* Display next term
+            displayTerm()
+        } else {
+            //User got the answer wrong so show appropriate animation
+            animateAnswer(false)
+        }
+    }
+
+    private suspend fun advance() = coroutineScope {
+        delay(1000L)
+        TermDisplayManager.loadNextTerm()
+    }
+
     private fun resetUI() {
-        //Put the edittext drawable back to the default blue
-        //Empty the contents of the edittext
+        findViewById<EditText>(R.id.answerbox).text.clear()
     }
 
 
     private fun animateAnswer(correct: Boolean) {
         val edittext = findViewById<EditText>(R.id.answerbox)
-        var animator: ObjectAnimator? = null
-        if (correct) {
-            animator = ObjectAnimator.ofInt(
-                edittext,
-                "background",
-                R.drawable.rounded_corners_textbox_bg_green
-            )
-        } else {
-            animator = ObjectAnimator.ofInt(
-                edittext,
-                "background",
-                R.drawable.rounded_corners_textbox_bg_red
-            )
-        }
-
-        animator.start()
-        edittext.invalidate()
+            if (correct) {
+                edittext.setBackgroundResource(R.drawable.rounded_corners_textbox_bg_green)
+            } else {
+                edittext.setBackgroundResource(R.drawable.rounded_corners_textbox_bg_red)
+                val shake: Animation =
+                    AnimationUtils.loadAnimation(this@ReviewTermActivity, R.anim.shake)
+                edittext.startAnimation(shake)
+            }
     }
 
 
